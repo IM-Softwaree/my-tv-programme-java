94a46623c8052b7b53e15248e3f0a8b5f4423642
Evangelia <evangelai@csd.auth.gr>
2024-01-01 13:58:04 +0200

    a

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,23 @@
  * ************* ΜΗ ΣΒΗΣΕΤΕ ΑΥΤΗ ΤΗΝ ΚΛΑΣΗ ************
  */
 
+import api.*;
+
 public class Main {
     public static void main(String[] args) {
 
         // ADD YOUR CODE HERE
+
+        //Initialization of files Admins.txt and Subscribers.txt
+        FileReaderForPersons fileReader = new FileReaderForPersons();
+
+        /**
+         //Send as a parameter the name of the file where you want to write
+         PersonAdderToFile fileWriter = new PersonAdderToFile("Admins.txt", "Subscribers.txt");
+         */
+
+        //Initialization of files Movies.dat and Series.dat
+        InitializationOfFiles in= new InitializationOfFiles();
 
     }
 }
