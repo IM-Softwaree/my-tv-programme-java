7d5d9b642726b2fbe88174a7d2aad44516efa640
Ioakeim Mertekis <mertekis@csd.auth.gr>
2024-01-01 13:37:18 +0200

    Commit

---
+++ src/api/FirstInitializationForAdminsSubscribers.java
@@ -0,0 +1,9 @@
+package api;
+
+public class FirstInitializationForAdminsSubscribers {
+    //Initialization of files Admins.txt and Subscribers.txt
+    FileReaderForPersons fileReader = new FileReaderForPersons();
+
+    //Send as a parameter the name of the file where you want to write
+    PersonAdderToFile fileWriter = new PersonAdderToFile("Admins.txt", "Subscribers.txt");
+}
