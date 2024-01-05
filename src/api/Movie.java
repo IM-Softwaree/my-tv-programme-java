63b6a74caaf4092af9aca5c9ace9ed15a5ab824c
Evangelia <evangelai@csd.auth.gr>
2024-01-06 01:42:15 +0200

    a little javadoc

--- src/api/Movie.java
+++ src/api/Movie.java
@@ -1,38 +1,75 @@
 package api;
 
-
 import java.io.*;
 import java.util.ArrayList;
 
+/**
+ * Class that implements a movie. It inherits fields and functions from the class Video. As additional fields has
+ * the year of its first appearance and its duration in minutes.
+ */
 public class Movie extends Video
 {
     private int yearOfFirstAppearance;
     private int movieDuration;
 
+    /**
+     * Constructor of a Movie with a call to the constructor of the superclass Video
+     * @param t The title of the Movie
+     * @param d The description of the Movie
+     * @param a The appropriateness level of the Movie
+     * @param c The category of the Movie
+     * @param p The protagonists of the Movie
+     * @param y The year of first appearance of the Movie
+     * @param m The duration in minutes of the Movie
+     */
     public Movie(String t,String d,String a,String c,String p,int y, int m){
         super(t,d,a,c,p);
         this.yearOfFirstAppearance=y;
         this.movieDuration=m;
     };
 
+    /**
+     * Function that sets the year of first appearance of the Movie
+     * @param x The year of first appearance of the Movie
+     */
     public void setYearOfFirstAppearance(int x)
     {
         this.yearOfFirstAppearance=x;
     }
+
+    /**
+     * Function that sets the duration in minutes of the Movie
+     * @param x The duration in minutes of the Movie
+     */
     public void setMovieDuration(int x)
     {
         this.movieDuration=x;
     }
 
+    /**
+     * Function that gets the year of first appearance of the Movie
+     * @return The year of first appearance of the Movie
+     */
     public int getYearOfFirstAppearance()
     {
         return this.yearOfFirstAppearance;
     }
+
+    /**
+     * Function that gets the duration in minutes of the Movie
+     * @return The duration in minutes of the Movie
+     */
     public int getMovieDuration()
     {
         return this.movieDuration;
     }
 
+    /**
+     * Function that appends an object Movie to the binary file Movies.dat. It returns true if the appending was
+     * successful and false if it was not, meaning that another object Movie with the same title already existed in the file.
+     *
+     * @return
+     */
     public boolean addMovieToFile()
     {
         boolean result=true;  //theoro oti i tainia den iparxei
--- src/api/Video.java
+++ src/api/Video.java
@@ -3,6 +3,10 @@ package api;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * Class that implements a video. A Video consists of a title, a description, an appropriateness level, a category,
+ * the protagonists and a list of its assessments.
+ */
 public class Video implements Serializable {
 
     private String title;
@@ -12,9 +16,21 @@ public class Video implements Serializable {
     private String protagonists;
 
     private ArrayList<Assessment> assessments = new ArrayList<>();
-    private float averageRatingForSearch;
+    private float averageRatingForSearch; //Field used only for search purposes
 
+    /**
+     * Empty constructor
+     */
     public Video(){}
+
+    /**
+     *Constructor of a Video
+     * @param t The title of the Video
+     * @param d The description of the Video
+     * @param a The appropriateness level of the Video
+     * @param c The category of the Video
+     * @param p The protagonists of the Video
+     */
     public Video(String t,String d,String a,String c,String p){
         this.title=t;
         this.description=d;
@@ -23,32 +39,64 @@ public class Video implements Serializable {
         this.protagonists =p;
     }
 
+    /**
+     * Function that sets the title of the Video
+     * @param x The title of the Video
+     */
     public void setTitle(String x)
     {
         this.title=x;
     }
+
+    /**
+     * Function that sets the description of the Video
+     * @param x The description of the Video
+     */
     public void setDescription(String x)
     {
         this.description=x;
     }
+
+    /**
+     * Function that sets the appropriateness of the Video
+     * @param x The appropriateness of the Video
+     */
     public void setAppropriateness(String x)
     {
         this.appropriateness=x;
     }
+
+    /**
+     * Function that sets the category of the Video
+     * @param x The category of the Video
+     */
     public void setCategory(String x)
     {
         this.category=x;
     }
+
+    /**
+     * Function that sets the protagonists of the Video
+     * @param x The protagonists of the Video
+     */
     public void setProtagonists(String x)
     {
         this.protagonists=x;
     }
+
+    /**
+     * Function that sets the average rating for search purposes of the Video
+     * @param x The average rating for search of the Video
+     */
     public void setAverageRatingForSearch(float x)
     {
         this.averageRatingForSearch=x;
     }
 
-
+    /**
+     * Function that finds the true average rating of the Video
+     * @return the average rating of the Video
+     */
     public float findAverageRating()
     {
         int sum=0;
@@ -58,35 +106,72 @@ public class Video implements Serializable {
         return (float) sum/ assessments.size();
     }
 
+    /**
+     * Function that gets the title of the Video
+     * @return the title of the Video
+     */
     public String getTitle(){
         return this.title;
     }
 
+    /**
+     * Function that gets the description of the Video
+     * @return the description of the Video
+     */
     public String getDescription(){
         return this.description;
     }
 
+    /**
+     * Function that gets the appropriateness of the Video
+     * @return the appropriateness of the Video
+     */
     public String getAppropriateness(){
         return this.appropriateness;
     }
 
+    /**
+     * Function that gets the category of the Video
+     * @return the category of the Video
+     */
     public String getCategory(){
         return this.category;
     }
 
+    /**
+     * Function that gets the protagonists of the Video
+     * @return the protagonists of the Video
+     */
     public String getProtagonists(){
         return this.protagonists;
     }
+
+    /**
+     * Function that gets the average rating for search purposes of the Video
+     * @return the protagonists of the Video
+     */
     public float getAverageRatingForSearch()
     {
         return this.averageRatingForSearch;
     }
+
+    /**
+     * Function that adds a new assessment to the list of the assessments of the Video
+     * @param username The name of the subscriber making the assessment
+     * @param text The comments of the subscriber making the assessment
+     * @param rating The rating given by the subscriber making the assessment
+     * @param date The date when the subscriber makde the assessment
+     */
     public void addAssessment(String username,String text, int rating, String date)
     {
         Assessment temp = new Assessment(username,text,rating,date);
         this.assessments.add(temp);
     }
 
+    /**
+     * Function that returns a list of similar Videos
+     * @return a list of similar Videos
+     */
     public ArrayList<Video> similarVideos()
     {
         ArrayList<Video> similar = new ArrayList<>();
