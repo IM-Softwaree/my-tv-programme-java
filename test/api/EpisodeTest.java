3e5a4bebdfc4a0b72fe80a0e665ab2fe74c3f204
Evangelia <evangelai@csd.auth.gr>
2024-01-07 23:50:24 +0200

    testakia

---
+++ test/api/EpisodeTest.java
@@ -0,0 +1,42 @@
+package api;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EpisodeTest {
+
+    @Test
+    public void testConstructor() {
+        String expectedName = "Test";
+        int expectedDuration = 0;
+
+        Episode testEpisode = new Episode(expectedName,expectedDuration);
+
+        assertEquals(expectedName, testEpisode.getName());
+        assertEquals(expectedDuration, testEpisode.getDuration());
+    }
+
+    @Test
+    public void testgetName() {
+        String expectedName = "Test";
+        int expectedDuration = 0;
+
+        Episode testEpisode = new Episode(expectedName,expectedDuration);
+
+        assertEquals(expectedName, testEpisode.getName());
+    }
+
+    @Test
+    public void testgetDuration() {
+        String expectedName = "Test";
+        int expectedDuration = 0;
+
+        Episode testEpisode = new Episode(expectedName,expectedDuration);
+
+        assertEquals(expectedDuration, testEpisode.getDuration());
+    }
+
+}
